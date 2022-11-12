c507f6e29e70ef1cd27f4e4f57b1229578cb802c
pritthwiraz <pritthwiraz@gmail.com>
2022-11-12 20:29:21 +0600

    api updated

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config();
 
 const app =express();
@@ -26,8 +26,16 @@ async function run(){
         app.get('/services', async(req, res) =>{
             const query = {}
             const cursor = serviceCollection.find(query);
-            const services = await cursor.limit(3).toArray();
+            const services = await cursor.toArray();
             res.send(services);
+        });
+
+        app.get('/services/:id', async(req,res) =>{
+            const id = req.params.id;
+            const query = {_id: ObjectId(id)};
+            const service = await serviceCollection.findOne(query);
+            res.send(service);
+
         })
     }
     finally{
